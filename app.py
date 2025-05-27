23168fb61642d8fd8e3bb6381abe864eda4d7399
S48avio <151058808+S48avio@users.noreply.github.com>
2025-05-27 15:54:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import numpy as np
 import cv2
 import os
-port = int(os.environ.get('PORT', 5000))  # default to 5000 for local dev
+  # default to 5000 for local dev
 
 
 
@@ -46,4 +46,5 @@ def predict():
     return jsonify({"detections": detections})
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
